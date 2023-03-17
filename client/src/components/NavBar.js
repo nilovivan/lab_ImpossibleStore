196eea7146fa56ab3559bd43b51fb107143b6733
RavenHacker1337 <aleksandr.rusanov.00@mail.ru>
2023-03-17 06:03:03 +0300

    Add XSS

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,7 +3,7 @@ import {Context} from "../index";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import {NavLink} from "react-router-dom";
-import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE, BASKET_ROUTE} from "../utils/consts";
 import {Button} from "react-bootstrap";
 import {observer} from "mobx-react-lite";
 import Container from "react-bootstrap/Container";
@@ -33,7 +33,14 @@ const NavBar = observer(() => {
                     <Nav className="ml-auto" style={{color: 'white'}}>
                         <Button
                             variant={"outline-light"}
+                            onClick={() => history.push(BASKET_ROUTE)}
+                        >
+                            Корзина
+                        </Button>
+                        <Button
+                            variant={"outline-light"}
                             onClick={() => history.push(ADMIN_ROUTE)}
+                            className="ml-2"
                         >
                             Админ панель
                         </Button>
