acc0d825f0b3db158ced2edd7458448565597305
Ivan <niilovivan@gmail.com>
2023-04-06 16:18:58 +0300

    docker

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -31,7 +31,7 @@ class UserController {
         }
         const hashPassword = await bcrypt.hash(password, 5)
         const user = await User.create({email, role, password: hashPassword})
-        const cart = await Cart.create({userId: user.id})
+        await Cart.create({userId: user.id})
         const token = generateJWT(user.id, user.email, user.role)
         return res.json({token})
     }
