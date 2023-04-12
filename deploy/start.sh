bf361470146029e0c596a6dc2b87e163bb2ef2fc
Ivan <niilovivan@gmail.com>
2023-04-12 23:10:51 +0300

    start script fixed

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 docker-compose build
 docker-compose up &
-docker cp ../src/db/backup.sql postgresql:/db.sql
+sleep 20
+RET=$(docker cp ../src/db/backup.sql postgresql:/db.sql 2>&1)
+while [ ${RET} = 'Error: No such container:path: postgresql:/' ]
+do
+	echo 'waiting..'
+	echo ${RET}
+	sleep 5
+	RET=$(docker cp ../src/db/backup.sql postgresql:/db.sql 2>&1)
+done
 docker exec -i postgresql pg_restore -d postgres db.sql
